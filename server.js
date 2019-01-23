ddd12b7cbad9c74c707939a33b8cffd079d55f04
Syahrul <pauscrypto@gmail.com>
2019-01-23 22:24:51 +0800

    seperate server from init

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static("public"))
+//Initialize the app
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  })
