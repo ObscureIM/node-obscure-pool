0c98e2e81f600359269fda383f0400c5abc52ee6
Syahrul <pauscrypto@gmail.com>
2019-01-25 09:17:14 +0800

    Changed IP

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-   res.setHeader("Content-Type", "application/json");
+
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
       res.send(200);
@@ -30,40 +30,29 @@ var server = app.listen(process.env.PORT || 8080, function () {
     console.log("App now running on port", port);
   })
 
-var url = 'http://pool.obscure.im:8117'
+var url = 'http://0.0.0.0:8117'
 app.get('/stats',function(req,res) {
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+  var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var request = new XMLHttpRequest()
-url2 = url + '/stats'
-request.open('GET', url2 , true);
-request.onload = function() {
-  if (request.status >= 200 && request.status < 400) {
-    // Success!
-    var data = JSON.parse(request.responseText);
-    res.send(data)
-  } else {
-    // We reached our target server, but it returned an error
-    console.log("fail")
-  }
-};
-request.send()
+  var request = new XMLHttpRequest()
+  url2 = url + '/stats'
+  request.open('GET', url2 , true);
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      // Success!
+      var data = JSON.parse(request.responseText);
+      res.send(data)
+    } else {
+      // We reached our target server, but it returned an error
+      console.log("fail")
+    }
+  };
+  request.send()
 })
+app.get('/stats_address') {
+  //make a capp to /stats?address =
+  address = req.query.address
+  var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+  var request = new XMLHttpRequest()
 
-app.get('/live_stats',function(req,res) {
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
-var request = new XMLHttpRequest()
-request.open('GET', 'http://pool.obscure.im:8117/live_stats' , true);
-request.onload = function() {
-  if (request.status >= 200 && request.status < 400) {
-    // Success!
-    console.log(request.responseText)
-    res.send(request.responseText)
-  } else {
-    // We reached our target server, but it returned an error
-    console.log("fail")
-  }
-};
-request.send()
-})
+}
